4793e5d8710985f7229f838add38ba24698ae91f
topopelon <jlaguna98@gmail.com>
2022-06-30 10:14:08 +0100

    dev nginx ready no https

--- dev.init.sh
+++ dev.init.sh
@@ -5,3 +5,5 @@ cd digital-twin-panel;
 yarn build-dev;
 cd ..;
 docker-compose -f docker-compose.dev.yml up --build;
+
+# On MacOS: launchctl load /usr/local/cellar/nginx/1.23.0/homebrew.mxcl.nginx.plist
