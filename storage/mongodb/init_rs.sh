9ff18d3ac5bb84e3317593ffa25dff0a1a0ef16a
topopelon <jlaguna98@gmail.com>
2024-10-09 17:08:32 +0100

    fix deployment in vps

--- storage/mongodb/init_rs.sh
+++ storage/mongodb/init_rs.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" <<EOF
+set -e
+
+until mongosh --eval "print(\"waited for connection\")"
+do
+    sleep 2
+done
+
+mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --port "$MONGO_PORT" <<EOF
 rs.initiate(
   {
     _id: "rs0",
-    members: [{ _id: 0, host: "mongo:27017" }],
+    members: [{ _id: 0, host: "mongo:${MONGO_PORT}" }],
   },
   { force: true }
 );
