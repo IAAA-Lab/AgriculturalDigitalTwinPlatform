513e66b10ef5914bd3f51a9f8cd8f36358c7e27c
topopelon <jlaguna98@gmail.com>
2024-10-09 17:59:12 +0000

    fixed stuff

--- project-cli.sh
+++ project-cli.sh
@@ -5,7 +5,7 @@
 ## Set the path to the cli
 PATH_CLI=src/internal/adapters/primary/cli
 ## Find the container name which contains the "gin-gonic" string
-DOCKER_LIST=$(docker ps -a --format "{{.Names}}")
+DOCKER_LIST=$(docker ps --format "{{.Names}}")
 CONTAINER_NAME=$(echo "${DOCKER_LIST}" | grep "gin-gonic")
 ARGS="$@"
 ## Execute the cli command with indefinite number of arguments within docker container
